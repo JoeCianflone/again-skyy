d9f9db8e0f2d85558b79bece73816a97b803fb32
Joe Cianflone <joe@cianflone.co>
2014-05-28 15:53:17 -0400

    adding some basic configuration files

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/public"
+css_dir = "public/assets/css"
+sass_dir = "public/assets/src/sass"
+images_dir = "public/assets/imgs"
+javascripts_dir = "public/assets/js"
+fonts_dir = "public/assets/fonts"
+
+output_style = :compressed
+
+relative_assets = true
+
+line_comments = false
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    gulpIf = require('gulp-if'),
+    gulpWatch = require('gulp-watch'),
+    gulpCompass = require('gulp-compass'),
+    gulpConcat = require ('gulp-concat'),
+    gulpUglify = require('gulp-uglify');
+
+gulp.task('default', function() {});
