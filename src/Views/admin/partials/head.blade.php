b087c40bc6f3caccb9616e83bd004f5f358d97cf
Joe Cianflone <joe@cianflone.co>
2014-06-09 17:47:04 -0400

    remove origin

--- src/Views/admin/partials/head.blade.php
+++ src/Views/admin/partials/head.blade.php
@@ -2,7 +2,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>@yield('title', 'Skyy | Again Admin Panel')</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="x-frame-options" content="allowall">
 <link rel="stylesheet" href="/assets/css/admin.css">
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet'>
