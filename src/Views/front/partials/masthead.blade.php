2dc9ad79423315427ca83e93d91eaf698b2f5f3e
Joe Cianflone <joe@cianflone.co>
2014-06-12 18:20:02 -0400

    padding:

--- src/Views/front/partials/masthead.blade.php
+++ src/Views/front/partials/masthead.blade.php
@@ -1,7 +1,7 @@
 <header class="masthead row">
   <div class="col-md-10 col-md-offset-1">
      <h1 class="logo text-hide">Skyy Stream \ Boiler Room</h1>
-     <p class="text-center">We're teaming up with Boiler Room to bring you a series of 6 broadcasts, showcasing an amazing selection of artists we're thrilled to be working with across New York, Los Angeles and San Francisco &ndash; all brought to you via clear HD SKYY Stream technology.</p>
+     <p class="text-center" style="padding: 0;">We're teaming up with Boiler Room to bring you a series of 6 broadcasts, showcasing an amazing selection of artists we're thrilled to be working with across New York, Los Angeles and San Francisco &ndash; all brought to you via clear HD SKYY Stream technology.</p>
       <ul class="list-inline text-center social-masthead">
          <li><a href="//www.facebook.com/skyyvodka" target="_blank"><span class="fa fa-facebook-square"><span class="text-hide">Facebook</span></span></a></li>
          <li><a href="//twitter.com/skyyvodka" target="_blank"><span class="fa fa-twitter"><span class="text-hide">Twitter</span></span></a></li>
